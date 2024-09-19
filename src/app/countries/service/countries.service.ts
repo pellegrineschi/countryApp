8ab10d7edbcafe1c9d2f3c971f29a61ec82aef67
pellegrineschi <pellegrineschi86@gmail.com>
2024-09-19 20:24:08 -0300

    debounce

--- src/app/countries/service/countries.service.ts
+++ src/app/countries/service/countries.service.ts
@@ -16,7 +16,7 @@ export class CountriesService {
     return this.httpClient.get<Country[]>(url)
      .pipe(
         catchError(() => of([])),
-        delay(2000)
+        // delay(2000)
       );
   }
 
